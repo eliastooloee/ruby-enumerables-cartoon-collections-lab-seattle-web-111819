71bb32dd1eeb5749aee5d4089b4d30c41ae2ff9d
Elias Tooloee <elitooloee@gmail.com>
2019-11-14 11:23:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(maybe_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  maybe_cheese.find do
+  
 end
